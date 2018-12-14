cc9fd93d77c630925120e0d013cdc324ab4dd92e
Evan Haldane <evanhaldane@gmail.com>
2018-12-14 14:50:56 -0500

    add day 5 part 2 solution

--- day5.py
+++ day5.py
@@ -1,14 +1,28 @@
+import string
+import re
+
 with open('input5.txt', 'r') as f:
     polymer = f.read()
 
-i = 0
+# react all substrings and return length of result
+def length_after_reacting(polymer):
+    i = 0
+    while i < len(polymer)-1:
+        if polymer[i].swapcase() == polymer[i+1]:
+            polymer = polymer[:i] + polymer[i+2:]
+            i = max(0, i-1)
+        else:
+            i += 1
 
+    return len(polymer)
 
-while i < len(polymer)-1:
-    if polymer[i].swapcase() == polymer[i+1]:
-        polymer = polymer[:i] + polymer[i+2:]
-        i = max(0, i-1)
-    else:
-        i += 1
+resulting_lengths = {}
 
-print(len(polymer))
+for char in string.ascii_lowercase:
+    pattern = char +'|'+ char.swapcase()
+    # remove all instances of that character, upper and lower
+    stripped = re.sub(pattern, '', polymer)
+    resulting_lengths[char] = length_after_reacting(stripped)
+
+# print minimum resulting length
+print(min(resulting_lengths.values()))
